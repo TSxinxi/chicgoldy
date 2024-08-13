23ddf87a1ac44a3c29b0d5d933ed9c63c9124de4
enxiu <hd1783260996@163.com>
2024-08-13 13:51:58 +0800

    初始化

--- server.js
+++ server.js
@@ -1,27 +1,44 @@
-// @ts-ignore
 // Virtual entry point for the app
-import * as remixBuild from 'virtual:remix/server-build';
-import {storefrontRedirect} from '@shopify/hydrogen';
-import {createRequestHandler} from '@shopify/remix-oxygen';
-import {createAppLoadContext} from '~/lib/context';
+import * as remixBuild from '@remix-run/dev/server-build';
+import {createRequestHandler, getBuyerIp} from '@shopify/remix-oxygen';
+import {createStorefrontClient, storefrontRedirect} from '@shopify/hydrogen';
+import {HydrogenSession} from '~/lib/session.server';
+import {getLocaleFromRequest} from '~/lib/utils';
 
 /**
  * Export a fetch handler in module format.
  */
 export default {
-  /**
-   * @param {Request} request
-   * @param {Env} env
-   * @param {ExecutionContext} executionContext
-   * @return {Promise<Response>}
-   */
   async fetch(request, env, executionContext) {
     try {
-      const appLoadContext = await createAppLoadContext(
-        request,
-        env,
-        executionContext,
-      );
+      /**
+       * Open a cache instance in the worker and a custom session instance.
+       */
+      if (!env?.SESSION_SECRET) {
+        throw new Error('SESSION_SECRET environment variable is not set');
+      }
+
+      const waitUntil = (p) => executionContext.waitUntil(p);
+      const [cache, session] = await Promise.all([
+        caches.open('hydrogen'),
+        HydrogenSession.init(request, [env.SESSION_SECRET]),
+      ]);
+
+      /**
+       * Create Hydrogen's Storefront client.
+       */
+      const {storefront} = createStorefrontClient({
+        cache,
+        waitUntil,
+        buyerIp: getBuyerIp(request),
+        i18n: getLocaleFromRequest(request),
+        publicStorefrontToken: env.PUBLIC_STOREFRONT_API_TOKEN,
+        privateStorefrontToken: env.PRIVATE_STOREFRONT_API_TOKEN,
+        storeDomain: `https://${env.PUBLIC_STORE_DOMAIN}`,
+        storefrontApiVersion: env.PUBLIC_STOREFRONT_API_VERSION || '2023-01',
+        storefrontId: env.PUBLIC_STOREFRONT_ID,
+        requestGroupId: request.headers.get('request-id'),
+      });
 
       /**
        * Create a Remix request handler and pass
@@ -30,17 +47,10 @@ export default {
       const handleRequest = createRequestHandler({
         build: remixBuild,
         mode: process.env.NODE_ENV,
-        getLoadContext: () => appLoadContext,
+        getLoadContext: () => ({cache, session, waitUntil, storefront, env}),
       });
 
       const response = await handleRequest(request);
-
-      if (appLoadContext.session.isPending) {
-        response.headers.set(
-          'Set-Cookie',
-          await appLoadContext.session.commit(),
-        );
-      }
 
       if (response.status === 404) {
         /**
@@ -48,11 +58,7 @@ export default {
          * If the redirect doesn't exist, then `storefrontRedirect`
          * will pass through the 404 response.
          */
-        return storefrontRedirect({
-          request,
-          response,
-          storefront: appLoadContext.storefront,
-        });
+        return storefrontRedirect({request, response, storefront});
       }
 
       return response;
